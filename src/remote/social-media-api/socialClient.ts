b176dcd48942cc53d7c148a8e774280ec01b816d
Josiah S <josiah124@revature.net>
2022-11-11 16:52:35 -0500

    added beanstalk connection

--- src/remote/social-media-api/socialClient.ts
+++ src/remote/social-media-api/socialClient.ts
@@ -4,7 +4,10 @@ import axios from 'axios';
 // Very simple, but it also doesn't give us much abstraction
 const socialClient = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:8080',
+  
+  //http://localhost:8080
+  //http://project3-env.eba-fufsnv9d.us-east-1.elasticbeanstalk.com
+  baseURL: 'http://project3-env.eba-fufsnv9d.us-east-1.elasticbeanstalk.com',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:3000',
