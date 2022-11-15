13f7e4fb8c8e005cc2504f506ffc19f40229ae6f
Josiah S <josiah124@revature.net>
2022-11-14 22:15:17 -0500

    working on deleting comments

--- src/remote/social-media-api/socialClient.ts
+++ src/remote/social-media-api/socialClient.ts
@@ -4,18 +4,12 @@ import axios from 'axios';
 // Very simple, but it also doesn't give us much abstraction
 const socialClient = axios.create({
   withCredentials: true,
-<<<<<<< HEAD
-  baseURL: 'http://project3-env.eba-fufsnv9d.us-east-1.elasticbeanstalk.com/',
-=======
   
   //http://localhost:8080
   //http://project3-env.eba-fufsnv9d.us-east-1.elasticbeanstalk.com
-<<<<<<< HEAD
-  baseURL: 'http://project3-env.eba-fufsnv9d.us-east-1.elasticbeanstalk.com',
->>>>>>> profile
-=======
+
   baseURL: 'http://localhost:8080',
->>>>>>> main
+
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:8080',
