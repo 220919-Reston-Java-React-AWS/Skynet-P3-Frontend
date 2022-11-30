527699d6840a61f135abd0573668f87be53f1d92
Yasin Adam <74yadam@gmail.com>
2022-11-30 17:45:12 -0500

    changed cloud link because of dns error

--- src/remote/social-media-api/socialClient.ts
+++ src/remote/social-media-api/socialClient.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 // This is the configuration for sending HTTP Requests with Axios
-// Very simple, but it also doesn't give us much abstraction.
+// Very simple, but it also doesn't give us much abstraction
 const socialClient = axios.create({
   withCredentials: true,
   //http://localhost:8080
-  //http://project3-env.eba-fufsnv9d.us-east-1.elasticbeanstalk.com
+  //http://ec2-54-196-94-234.compute-1.amazonaws.com
 
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://ec2-54-196-94-234.compute-1.amazonaws.com',
 
   headers: {
     'Content-Type': 'application/json',
